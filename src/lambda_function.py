5b2f0843d1ffa494138e284d987d7246d9fbd750
Reyes2777 <jonathan.reyes2777@gmail.com>
2018-09-22 13:30:52 -0500

    cambios

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,25 +1,29 @@
-import time
-
-from webdriver_wrapper import WebDriverWrapper
-
+from selenium import webdriver
+import os
 
 def lambda_handler(*args, **kwargs):
-    driver = WebDriverWrapper()
 
-    driver.get_url('https://www.google.es/')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1280x1696')
+    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+    chrome_options.add_argument('--hide-scrollbars')
+    chrome_options.add_argument('--enable-logging')
+    chrome_options.add_argument('--log-level=0')
+    chrome_options.add_argument('--v=99')
+    chrome_options.add_argument('--single-process')
+    chrome_options.add_argument('--data-path=/tmp/data-path')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--homedir=/tmp')
+    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
 
-    driver.set_input_value('//input[@id="lst-ib"]', '21 buttons')
-
-    driver.click('//center//img[@alt="Google"]')
-    time.sleep(0.5)
-
-    driver.click('//input[@name="btnK"]')
-    time.sleep(0.5)
-
-    first_google_result_title = driver.get_inner_html('(//div[@class="rc"]//a)[1]')
-
-    print("--------------------------")
-    print(first_google_result_title)
-    print("--------------------------")
-
-    driver.close()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("https://www.procuraduria.gov.co/portal/consulta_antecedentes.page")
+    title = driver.title
+    print ("El Titulo de la pagina es: ",title)
+    
+    driver.quit()
