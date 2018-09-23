c07448b4c5fdf1028ef45efc1a4b7784b46ccfcc
Reyes2777 <jonathan.reyes2777@gmail.com>
2018-09-22 20:02:52 -0500

    changes

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,28 +1,25 @@
 from selenium import webdriver
+from webdriver_wrapper import chrome_options
+import json
 import os
 
-def lambda_handler(*args, **kwargs):
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1280x1696')
-    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-    chrome_options.add_argument('--hide-scrollbars')
-    chrome_options.add_argument('--enable-logging')
-    chrome_options.add_argument('--log-level=0')
-    chrome_options.add_argument('--v=99')
-    chrome_options.add_argument('--single-process')
-    chrome_options.add_argument('--data-path=/tmp/data-path')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--homedir=/tmp')
-    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-    chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+def lambda_handler(event,contex):
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.procuraduria.gov.co/portal/consulta_antecedentes.page")
+    driver.get("https://www.facebook.com/")
     title = driver.title
-    print ("El Titulo de la pagina es: ",title)    
+    print ("El Titulo de la pagina es: ",title)
+    usr = driver.find_element_by_xpath("//*[@id='email']") 
+    usr.send_keys(["usr"])
+    passwd = driver.find_element_by_xpath("//*[@id='pass']")
+    passwd.send_keys(["passwsd"])
+    driver.find_element_by_xpath("//*[@id='u_0_2']").click()
+    driver.find_element_by_xpath("//li[@id='navItem_588143690']/a/div").click()
+    title = driver.title
+    print (title)
     driver.quit()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps('Funciona!!!')
+    }
